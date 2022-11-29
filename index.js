a10c01b39363dbd51fb874a93a7d1c397a4f37b4
Tate <tatebraeckel@gmail.com>
2022-11-28 19:02:36 -0700

    commit tree 7f71ce14ce1ec1118004371484caa57163ababb7

--- index.js
+++ index.js
@@ -1,1 +1,93 @@
-// Write your solution in this file!
+let employee = {
+    name: '',
+    streetAddress: ''
+}
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    function beforeEach() {
+        for (const key in employee) {
+            delete employee[key];
+          }
+          employee.name = 'Sam';
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const employee = {
+//     name: `Sam`,
+//     streetAddress: `12 Broadway`
+// }
+
+// function  updateEmployeeWithKeyAndValue(employee, key, value) {
+
+
+// const employees = {...employee};
+// employees[key] = value;
+// return employees;
+
+// }
+
+// console.log(updateEmployeeWithKeyAndValue("Sam"))
+
+// function  destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+
+
+//     employee[key] = value;
+//     return employee;
+    
+//     }
+
+//     function  deleteFromEmployeeByKey(employee, key, value) {
+
+// const newEmployee = {...employee};
+// newEmployee[key] = value;
+// return newEmployee;
+
+//         }
+
+
+// function  destructivelyDeleteFromEmployeeByKey(employee, key) {
+
+//             const newEmployee = {};
+    
+//          delete employee[key];
+//         return employee;
+                        
+//                                 }
+
